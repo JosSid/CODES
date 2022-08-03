78b938f25373a4b4af6073a3ad968df4399a9080
Josep Valls <alfredvays@gmail.com>
2022-08-03 18:53:01 +0200

    Funciones basicas2

--- codes_JS/basicsJS/funciones.js
+++ codes_JS/basicsJS/funciones.js
@@ -25,3 +25,34 @@ const hello5 = name => `Hello ${name}`
 
 console.log(hello5('Josep'))
 
+//Otros Ejemplos
+/**
+ * @description Devuelve un String en formato `${nombre} es ${empleo}`
+ * @param {String} nombre 
+ * @param {String} empleo 
+ * @returns {String}
+ */
+function foo(nombre, empleo) {
+    return `${nombre} es ${empleo}`
+}
+
+let resultado1 = foo('Josep', 'Programador')
+resultado1 = foo('Pepe', 'Chapuzas')
+
+console.log(resultado1)
+
+const foo2 = function (nombre, empleo) {
+    return `${nombre} es ${empleo}`
+}
+
+console.log(foo2('Antonio', 'Panadero'))
+
+const foo3 = (nombre, empleo) => {
+    return `${nombre} es ${empleo}`
+}
+
+console.log(foo3('Manolo', 'Fontanero'))
+
+const foo4 = (nombre, empleo) => `${nombre} es ${empleo}`
+
+console.log(foo4('Ari', 'Profesora'))
