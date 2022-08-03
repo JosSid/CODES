0c17d6ff7177dbb4b161fbd795423ef1c0b455cf
Josep Valls <alfredvays@gmail.com>
2022-08-03 21:04:11 +0200

    Añado clase a callbacks

--- codes_JS/basicsJS/callbacks.js
+++ codes_JS/basicsJS/callbacks.js
@@ -1,4 +1,16 @@
-const legolas = {
+class Personaje {
+    constructor(name, attackSpeed = 10, attackDamage = 15) {
+        this.name = name
+        this.attackSpeed = attackSpeed
+        this.attackDamage = attackDamage
+    }
+    
+    addProp(prop, value) {
+        this[prop] = value
+    }
+}
+
+/* const legolas = {
     name: 'Legolas',
     attackSpeed: 20,
     attackDamage: 10
@@ -8,7 +20,12 @@ const gimly = {
     name: 'Gimly',
     attackSpeed: 7,
     attackDamage: 15
-}
+} */
+
+const legolas = new Personaje('Legolas', 20, 10)
+const gimly = new Personaje('Gimly', 7)
+const aragorn = new Personaje('Aragorn', 18, 22)
+aragorn.addProp('age', 38)
 
 function atacar(personaje, attackFunc) {
     let attack = attackFunc(personaje)
@@ -45,3 +62,5 @@ atacar(legolas, attackWithSword)
 atacar(legolas, attackWithBowAndArrow)
 atacar(gimly, attackWithSword)
 atacar(gimly, attackWithAxe)
+atacar(aragorn, attackWithBowAndArrow)
+console.log(aragorn)
