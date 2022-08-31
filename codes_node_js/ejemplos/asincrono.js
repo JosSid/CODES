8baf11dae7d4b4c50f277e6c478b53deffc66ee8
Josep Valls <alfredvays@gmail.com>
2022-08-31 19:25:40 +0200

    "actualizo asincrono con array"

--- codes_node_js/ejemplos/asincrono.js
+++ codes_node_js/ejemplos/asincrono.js
@@ -8,10 +8,34 @@ function escribeTras2Segundos(texto, callback){
         callback()
     }, 2000)
 };
+//Llamar a la funcion fn, n veces
+function serie(n, fn, callbackTerminacion){
+    if(n === 0){
+        callbackTerminacion();
+        return
+    }
+    n--
+    fn(`Texto ${n}`, function(){
+        serie(n, fn, callbackTerminacion);
+    });
+};
 
-escribeTras2Segundos('Texto 1', function(){
-    escribeTras2Segundos('Texto 2', function(){
-        console.log('Termino')
-    })
-});
+function serie2(arr, fn, callbackTerminacion){
+    if(arr.length === 0){
+        callbackTerminacion();
+        return
+    }
+    fn(`Texto ${arr.shift()}`, function(){
+        serie2(arr, fn, callbackTerminacion);
+    });
+}
 
+
+
+/* serie(6, escribeTras2Segundos, function(){
+    console.log('Termino')
+}); */
+
+serie2([1,2,3,4,5,6], escribeTras2Segundos, function(){
+    console.log('Termino')
+})
